118c70845ca08e401453005a366c637f2f818d49
aaronangxz <angxze@gmail.com>
2020-09-12 02:18:59 +0800

    Updated Tutorial03-Q1 comments

--- Tutorials/Tutorial03/Tutorial03-Q1.cpp
+++ Tutorials/Tutorial03/Tutorial03-Q1.cpp
@@ -2,15 +2,22 @@
 
 int f(int x, int y);
 
-int main(void) {
+int main(void) 
+{
     int x = 3, y = 4;
-    
+    //0. x = 3, y =4
+
     x = f(x,y);
+    //1. x = 3*10 + 4 = 34
+
     y = f(x, f(y,x));
+    //2. y = f(34, f(4,34)) = f(34, 74) = 34*4 + 714 = 414   
+
     printf("x = %d; y = %d\n", x, y);
     return 0;
 }
 
-int f(int x, int y) {
-    return x*10 + y;
+int f(int x, int y) 
+{
+    return x * 10 + y;
 }
