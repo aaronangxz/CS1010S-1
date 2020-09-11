a128177fdc70ed660ee380d943e438b419f88602
aaronangxz <angxze@gmail.com>
2020-09-12 02:14:56 +0800

    Updated Tutorial03-Q2 comments

--- Problem Sets/ProblemSet02/task2.cpp
+++ Problem Sets/ProblemSet02/task2.cpp
@@ -4,10 +4,11 @@
 double taxi_fare(int weekday, int start_time, int speed, int distance) 
 {
     printf("Distance: %d\n",distance);
-    int weekday_input, start_time_hours, start_time_mins, start_time_secs, dist_elapsed = 0,time_elapsed ;
-    double base_fare = 3.40, fare = 0, surcharge = 0, dist_remain = 0;
+    int weekday_input, start_time_hours, start_time_mins, start_time_secs, dist_elapsed = 0,time_elapsed=0, dist_remain = 0;
+    double base_fare = 3.40, fare = 0, surcharge = 0;
     char dayofweek[7][100] = {"Mon", "Tue","Wed","Thu","Fri","Sat","Sun"};
-    
+    int dist_count =0;
+    //Determin day of week base on int input
     if (weekday < 1 || weekday > 7)
     {
         printf("Invalid day!\n");
@@ -17,14 +18,14 @@ double taxi_fare(int weekday, int start_time, int speed, int distance)
         weekday = weekday;
         int n = weekday - 1;
         //printf("%s ", dayofweek[n]);
-
         start_time_hours = start_time / 60;
         start_time_mins = (start_time - (start_time_hours * 60));
         start_time_secs = 00;
         printf("Start: %s %d:%d\n",dayofweek[n],start_time_hours,start_time_mins);
     }
     
-   if (distance <= 1000)
+    /*
+    if (distance <= 1000)
     {
         base_fare = 3.40;
     }
@@ -35,9 +36,15 @@ double taxi_fare(int weekday, int start_time, int speed, int distance)
             base_fare = 3.40 + 0.22;
         }
         else
-        {
-            base_fare = 3.40 + (((distance - 1000) / 400) * 0.22);
-        }    
+        {    
+        while (distance > 1000)
+            {
+                distance -= 400;
+                fare += 0.22;
+                printf("Fare is : %f\n",fare);
+            } 
+            fare = fare + base_fare;
+        }
     }
     else if (distance > 10000)
     {
@@ -47,9 +54,17 @@ double taxi_fare(int weekday, int start_time, int speed, int distance)
         }
         else
         {
-            base_fare =  3.40 + 5.50 + (round((distance - 10000) / 350.0) * 0.22);
-        }    
+            while (distance > 10000)
+            {
+                distance -= 350;
+                fare += 0.22;
+                printf("Fare is : %f\n",fare);
+            } 
+            fare = fare + base_fare;
+        }
     }
+    */
+
     //Compute surcharge base on start time
     if (weekday >= 1 && weekday <= 5 && start_time >= 360 && start_time <= 539)
     {
@@ -70,21 +85,22 @@ double taxi_fare(int weekday, int start_time, int speed, int distance)
     }
     printf("The Surcharge is x %.3f\n",surcharge);
 
-    //Flag down fare
-    fare = base_fare * surcharge;
+    //When distance is less than & equals to 1000m
+   while (dist_count < distance)
+   {
+       dist_count += 500;
+        if (dist_count <= 1000)
+        {
+            fare = 3.40;
+        }
+        else if (dist_count > 1000 && dist_count <= 10000)
+        {
+            fare 
+        }
+   }
+   
     
-    dist_elapsed += 1000;
     
-    dist_remain = distance - dist_elapsed;
-    
-    time_elapsed = start_time + (dist_elapsed / 500); 
-    
-    printf("Distance elapsed: %d\n",dist_elapsed);
-    printf("Current fare: $%.3f\n",fare);
-    printf("Distance remaining: %.0f\n",dist_remain);
-    printf("Time elapsed: %d\n",time_elapsed);
-    
-
 
     return fare;
 }
@@ -92,7 +108,7 @@ double taxi_fare(int weekday, int start_time, int speed, int distance)
 int main(void) {
     // You may change the inputs to the function for testing
     //double fare = taxi_fare(1, 17*60 + 59, 6, 1000);
-    double fare1 = taxi_fare(1, 17*60 + 57, 6, 2000);
+    double fare1 = taxi_fare(1, 5*60 + 50, 6, 15000);
     //printf("\nThe taxi fare is: $%.3f", fare);
     printf("\nThe taxi fare is: $%.3f", fare1);
     return 0;
--- Tutorials/Tutorial03/Tutorial03-Q2.cpp
+++ Tutorials/Tutorial03/Tutorial03-Q2.cpp
@@ -3,20 +3,68 @@
 
 int print_10_to_b(int n, int b) 
 {
-    int converted[50];
+    //Convert number in base b to base 10
+    //Array for converted numbers
+    int convert[50];
     int i = 0;
 
+    //Loop until n is 0 
     while ( n != 0)
     {
-        converted[i] = n % b;
+        //Fill the ith location in array (Starts from 0) with remainder of n / base
+        //Essentially the last number of n        
+        convert[i] = n % b;
+        //Stripping off the last digit of n in every iteration
         n = n / b;
         i ++ ;
+
+        /*
+        ----- Start of loop -----
+
+        i = 0
+        convert[0] = 130 % 5 = 0
+        n = 130 / 5 = 26
+
+        i = 1
+        convert[1] = 26 % 5 = 1
+        n = 26 / 5 = 5
+
+        i = 2
+        convert[2] = 5 % 5 = 0
+        n = 5 / 5 = 1
+
+        i = 3
+        convert[3] = 1 % 5 = 1
+        n = 1 / 5 = 0
+
+        i = 4
+        ----- End of loop -----     
+
+        Values in array --> convert[] = {0,1,0,1} 
+
+        */
     }
+    //Going back one position as the this position in array (empty) is not relevant
+    // i = 4 - 1, so i goes back to 3 which is convert[3]
     i --;
     
+    //Start reverse looping from previous position of array (convert[3]), until i is < 0
     for(  ; i >= 0; i--)
     {
-	    printf("%d",converted[i]);
+	    printf("%d",convert[i]);
+
+        /*
+
+        ----- Start of loop -----
+        i = 3, prints 1
+        i = 2, prints 0
+        i = 1, prints 1
+        i = 0, prints 0
+        ----- End of loop -----
+
+        Result of print_10_to_b(130, 5) --> 1010
+
+        */
     }
     printf("\n");
     return 0;
@@ -26,13 +74,14 @@ int print_b1_to_b2(int n, int b1, int b2)
 {
     // Convert n from base b1 to base 10, 
     // then sub returned values (sum) to <print_10_to_b> function
-    
     int i = 0, sum = 0;
     
     while ( n!= 0)
     {
+        // Similar to <print_10_to_b> function
         int remainder = n % 10;
         n = n / 10;
+        //Each digit * base^corresponding power
         sum += remainder * pow(b1,i);
         i++ ;
 
@@ -40,6 +89,7 @@ int print_b1_to_b2(int n, int b1, int b2)
         Computation steps i.e. 
         Convert 130 base 5 to base 10
 
+        ----- Start of loop -----
         i = 0
         130 % 10 = 0
         n = 130 / 10 = 13
@@ -54,6 +104,7 @@ int print_b1_to_b2(int n, int b1, int b2)
         1 % 10 = 1
         n = 1 / 10 = 0
         sum = 0 * 5^0 + 3 * 5^1+ 1 * 5^2
+        ----- End of loop -----
 
         */
     }
