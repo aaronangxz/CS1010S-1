d22add7acb6846e959b128316fa41bb48ab6649c
aaronangxz <angxze@gmail.com>
2020-09-14 21:18:36 +0800

    Added Pset 03

---
+++ Problem Sets/ProblemSet03/task1.cpp
@@ -0,0 +1,44 @@
+/*************************
+ * Problem Set 3: Task 1 *
+ *************************
+ 
+Make sure you have answered ALL 
+the questions in this task.   */
+
+#include <stdio.h>
+
+
+// a.
+int factorial(int n) {
+    // your answer here     
+}
+
+
+/* b. 
+What happens to your function when n >= 13? Explain why.
+(Put your answer within the comments)
+Answer: 
+
+
+*/
+
+
+// c.
+int choose(int n, int k) {
+    // your answer here     
+}
+
+
+/* d. 
+Are you able to make use of the factorial function in choose? Explain why.
+(Put your answer within the comments)
+Answer: 
+
+
+*/
+
+
+// e. 
+void pascal_triangle(int row) {
+    // your code here
+}
---
+++ Problem Sets/ProblemSet03/task2.cpp
@@ -0,0 +1,38 @@
+/*************************
+ * Problem Set 3: Task 2 *
+ *************************
+ 
+Make sure you have answered ALL 
+the questions in this task.   */
+
+#include <stdio.h>
+
+
+// a.
+bool is_leap_year(int year) {
+    // your answer here     
+}
+
+
+// b. 
+int days_in_month(int month, int year) {
+    // your answer here
+}
+
+
+// c.
+int days_from_epoch(int day, int month, int year) {
+    // your answer here     
+}
+
+
+// d.
+int day_of_week(int day, int month, int year) {
+    // your answer here
+}    
+
+
+// e. 
+void display_month(int month, int year) {
+    // your code here
+}
