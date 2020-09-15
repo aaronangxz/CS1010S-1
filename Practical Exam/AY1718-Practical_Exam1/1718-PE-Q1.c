1349864c8a85eec15ee0161acd7c727fbcec1f8b
aaronangxz <angxze@gmail.com>
2020-09-15 14:40:27 +0800

    Updated AY1718 PE Q1

--- Practical Exam/AY1718-Practical_Exam1/1718-PE-Q1.c
+++ Practical Exam/AY1718-Practical_Exam1/1718-PE-Q1.c
@@ -2,14 +2,26 @@
 #include <stdbool.h>
 
 /* Question 1A */
-int sum_of_factors(int n ) {
-    // Your code here    
+int sum_of_factors(int n ) 
+{
+    int sum = 0;
+    for (int i = 1; i < n ; i++)
+    {
+        if (n % i == 0)
+        {
+            sum += i;
+        }
+    }
+    return sum;
 }
 
 /* Question 1B */
 bool is_lovely(int n1, int n2) {
-    // Your code here
-
+    if (sum_of_factors(n1)== n2 && sum_of_factors(n2)==n1)
+    {
+        return true;
+    }
+    return false;
 }
 
 int main(void) {
