c12be11487014d61989f78b9aef99fa7a01081ef
aaronangxz <angxze@gmail.com>
2020-09-15 18:13:28 +0800

    Updated AY1718 PE Q2

--- Practical Exam/AY1718-Practical_Exam1/1718-PE-Q2.c
+++ Practical Exam/AY1718-Practical_Exam1/1718-PE-Q2.c
@@ -2,14 +2,31 @@
 
 /* Question 2 */
 int num_payments(double principal, double interest, double factor, double payment) {
-    // Your code here
+    double remaining = principal;
+    int month = 0;
 
+    while (remaining >= 0)
+    {
+        remaining = principal * (1+(interest)) - payment;
+        //printf("Remaining: %f\n",remaining);
+        month++;
+        principal = remaining;
+        if (month % 12 == 0)
+        {
+            interest = interest * factor;
+        }
+        if (interest > payment)
+        {
+            return -1;
+        }
+    }
+    return month;
 }
 
 int main(void) {
     double principal = 1000000;
     double interest = 0.005;
-    double factor = 1.0
+    double factor = 1.01;
     double payment = 6000;
     printf("Number of payments: %d\n", num_payments(principal, interest, factor, payment));
 }
