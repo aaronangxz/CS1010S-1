7e31dc82658164cf51bac5d341b90b3b1222878d
aaronangxz <angxze@gmail.com>
2020-09-16 18:32:28 +0800

    Updated Pset03 Q1

--- Problem Sets/ProblemSet03/task1.cpp
+++ Problem Sets/ProblemSet03/task1.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
 /*************************
  * Problem Set 3: Task 1 *
  *************************
@@ -9,23 +13,46 @@ the questions in this task.   */
 
 
 // a.
-int factorial(int n) {
-    // your answer here     
+int factorial(int n) 
+{
+    int factorial = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+   // cout << "Factorial: " << factorial << endl;
+    return factorial;
+    
 }
 
+long factoriallong(int n) 
+{
+    long factorial = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+   // cout << "Factorial: " << factorial << endl;
+    return factorial;
+}
 
 /* b. 
 What happens to your function when n >= 13? Explain why.
 (Put your answer within the comments)
 Answer: 
 
+When n >= 13, value of n! will be out of int's scope.
 
 */
 
 
 // c.
-int choose(int n, int k) {
-    // your answer here     
+int choose(int n, int k) 
+{
+    
+    
+    int choose = factoriallong(n) / (factoriallong(k) * factoriallong(n-k));
+    return choose;
 }
 
 
@@ -33,12 +60,29 @@ int choose(int n, int k) {
 Are you able to make use of the factorial function in choose? Explain why.
 (Put your answer within the comments)
 Answer: 
-
+Yes, as n choose k = n! / (k!(n-k)!)
 
 */
 
 
 // e. 
-void pascal_triangle(int row) {
-    // your code here
+void pascal_triangle(int row) 
+{
+    int digits = row  ;
+    for ( int i = 0; i <= digits; i++)
+    {
+        int number = choose(row,i);
+        cout << number << " ";
+    }
+    // for ( int i = digits + 1; i >= 0; i--)
+    // {
+    //     int number = choose(row,i);
+    //     cout << number << " ";
+    // }
+    
+}
+
+int main (void)
+{
+    pascal_triangle(15); 
 }
