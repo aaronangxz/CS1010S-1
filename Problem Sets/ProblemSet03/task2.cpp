6bb97e30b26f423229691516f9721b6d993fd99b
aaronangxz <angxze@gmail.com>
2020-09-17 22:05:48 +0800

    Updated Pset03 Q2

--- Problem Sets/ProblemSet03/task2.cpp
+++ Problem Sets/ProblemSet03/task2.cpp
@@ -50,8 +50,38 @@ int days_in_month(int month, int year)
 }
 
 // c.
-int days_from_epoch(int day, int month, int year) {
-    // your answer here     
+int days_from_epoch(int day, int month, int year) 
+{
+    int daysum = 0;
+    int year_diff = year - 1970;
+    int month_diff = month - 1;
+    int day_diff = day - 1;
+
+    for (int i = year; i > 1970; i--)
+    {
+        if (is_leap_year(i))
+        {
+            daysum += 366;
+        }
+        else //if (year != 0 && is_leap_year(year)==0)
+        {
+            daysum += 365;
+        }
+    }
+    
+    cout << "1. Current Day: " << daysum << endl;
+
+    for (int i = 2; i <= month; i++)
+    {
+        daysum += days_in_month(i, year); 
+    }
+    cout << "2. Current Day: " << daysum << endl;
+
+    daysum += day_diff;
+    
+    cout << "3. Current Day: " << daysum << endl;
+    return daysum;
+         
 }
 
 
@@ -72,4 +102,7 @@ int main(void)
     cout << test << endl;
     int test1 = days_in_month(2,2000);
     cout << "There are " << test1 << " days." << endl;
+    int test2 = days_from_epoch(9, 8, 1965)	;
+    cout << "Epoch: " << test2 << endl;
+
 }
