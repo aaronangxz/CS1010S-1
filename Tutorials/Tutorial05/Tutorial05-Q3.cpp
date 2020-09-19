81ab470afb8c3cec0c4f649e6446d7898c64d2d2
aaronangxz <angxze@gmail.com>
2020-09-20 04:03:32 +0800

    Updated Tutorial05 Q3

---
+++ Tutorials/Tutorial05/Tutorial05-Q3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+void print_cells(char chara)
+{
+    printf("%c",chara);
+}
+
+void game_of_life(bool cells[], int size, int num_gen) 
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (cells[i] == 1)
+        {
+            //print_cells('X');
+            printf("X");
+        }
+        //print_cells('_');
+        printf("_");
+    }
+    
+}
+
+
+
+void next_gen (int cells)
+{
+
+}
+
+
+int main(void) 
+{
+    int cells[]={0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
+    //game_of_life({0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, 15, 9);
+    
+    /*
+    Testing ONLY! print of X _
+    for (int i = 0; i < 15; i++)
+    {
+        if (cells[i] == 1)
+        {
+            //print_cells('X');
+            printf("X");
+        }
+        else
+        {
+            //print_cells('_');
+        printf("_");
+        }
+        
+    }
+    */
+}
