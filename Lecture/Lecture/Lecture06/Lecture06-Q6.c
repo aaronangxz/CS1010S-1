85ccddd0b3d4beb2e8b81480cf6d9ac55b9086c0
aaronangxz <angxze@gmail.com>
2020-09-22 22:37:35 +0800

    Updated Lecture06 Q6 comments

--- Lecture/Lecture/Lecture06/Lecture06-Q6.c
+++ Lecture/Lecture/Lecture06/Lecture06-Q6.c
@@ -7,14 +7,22 @@
 
 void to_upper(char str[]) 
 {
+    //Count from first position (str[0]) to null (str[\0])
     for (int i = 0; str[i] !='\0';i++)
     {
+        //Minus 32 (ASCII lower case to upper case) when value in array is a to z inclusive.
         if(str[i] >= 'a' && str[i] <= 'z')
         { 
+            //Update the original array with new value
             str[i] = str[i] - 32; 
         }
     }
 }
+
+/*
+Getting this error -> /bin/sh: line 1:  7040 Bus error: 10 on VS Code.
+Somehow works on Coursemology and passed all test cases.
+*/
 
 int main(void) 
 {
