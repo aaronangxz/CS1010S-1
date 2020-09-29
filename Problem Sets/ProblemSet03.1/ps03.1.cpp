bcc2c9a6392a886b8ea394108b0c9c24909c80ca
aaronangxz <angxze@gmail.com>
2020-09-29 13:13:41 +0800

    Updated Pset03.1

---
+++ Problem Sets/ProblemSet03.1/ps03.1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void greg_to_mayan(int day, int month, int year) {
+    // your code here
+}
+
+void mayan_to_greg(int baktun, int katun, int tun, int uinal, int kin) {
+    // your code here
+}
+
+int main(void) {
+    greg_to_mayan(21, 12, 2012);
+    mayan_to_greg(13, 0, 0, 0, 0);
+    return 0;
+}
