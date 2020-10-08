4e00f5d0ce7b6d89bab6501de3a18c472986f94f
aaronangxz <angxze@gmail.com>
2020-10-08 23:22:11 +0800

    Updated Lecture 07 Q5

--- Lecture/Lecture/Lecture07/Lecture07-Q5.cpp
+++ Lecture/Lecture/Lecture07/Lecture07-Q5.cpp
@@ -5,20 +5,21 @@ using namespace std;
 
 vector<bool> And(vector<bool> v1, vector<bool> v2) 
 {
-    vector<bool> out = {};
-    for (int i = 0; (i < v1.size() && 1 < v2.size()); i++)
+    vector<bool> out;
+    
+    for (int i = 0; (i < v1.size()); i++)
     {
-        if (v1[i] == 1 && (v1[i] == v2[i]))
+        if (v1[i] == v2[i] && (v1[i] == 1 || v2[i] == 1) )
         {
-           out[i] = 1;
+           v1[i] = 1;
         }
         else
         {
-            out[i] = 0;
+            v1[i] = 0;
         }
     }
+    return v1;
 }
-
 
 int main (void)
 {
