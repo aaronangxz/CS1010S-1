34b64830b9f977046b919636ca539f370b01cf32
aaronangxz <angxze@gmail.com>
2020-10-10 13:51:32 +0800

    Added Tutorial 06 algo practice

---
+++ Tutorials/Tutorial06/Tutorial06-algopractice.cpp
@@ -0,0 +1,24 @@
+/*
+https://open.kattis.com/problems/hissingmicrophone
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (void)
+{
+    string input = "amisss";
+
+    for (int i = 0; i < input.length(); i++)
+    {
+        if (input[i] == 's' && input[i+1] == 's')
+        {
+            cout << "hiss" <<endl;
+            return 0;
+        }
+    }
+    cout << "No hiss" << endl;
+    return 0;
+}
