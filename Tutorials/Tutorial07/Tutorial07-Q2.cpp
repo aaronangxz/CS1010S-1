241c2c16da55e970c6c31ff087fc65582bd3ad27
aaronangxz <angxze@gmail.com>
2020-10-23 01:05:08 +0800

    Updated Tutorial 07

--- Tutorials/Tutorial07/Tutorial07-Q2.cpp
+++ Tutorials/Tutorial07/Tutorial07-Q2.cpp
@@ -4,24 +4,32 @@ using namespace std;
 
 void h(int& i) 
 {
+    cout << "input &i in h: " << &i << endl;
     i = 33; // point α
+    cout << "i in h: " << i << endl;
 }
 
 void g(int* i) 
 { 
+    cout << "input *i in g: " << *i << endl;
     *i = 22;
+    cout << "*i in g: " << *i << endl;
     h( *i );
+    cout << "i in g: " << i << endl;
 }
 
 void f(int i) 
 { 
+    cout << "input i in f: " << i << endl;
     i = 11;
+    cout << "&i in f: " << &i << endl;
     g( &i ); 
+    cout << "i in f: " << i << endl;
 }
 
 int main() 
 { 
     int i = 0;
     f( i ); 
-    cout << i;
+    cout << "i in main: " << i << endl;
 }
