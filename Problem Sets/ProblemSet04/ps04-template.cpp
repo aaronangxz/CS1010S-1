6d9ea938f02d74293bd112055298ef9d3a7f591f
aaronangxz <angxze@gmail.com>
2020-10-31 05:18:49 +0800

    Updated Pset 04

--- Problem Sets/ProblemSet04/ps04-template.cpp
+++ Problem Sets/ProblemSet04/ps04-template.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
@@ -11,16 +12,19 @@ using namespace std;
 char shift_char(char c, int n) 
 {
     bool isLower = (c >= 'a' && c <= 'z')? 1 : 0 ;
-    c += n % 26;
-
-    while (isLower && c < 'a')
-        c += 26;
-    while (isLower && c > 'z')
-        c -= 26;
-    while (!isLower && c > 'Z')
-        c -= 26;
-    while (!isLower && c < 'A')
-        c += 26;
+    c += n >= 0 ? n % 26 : n < 0 ? - (abs(n) % 26) : 0;
+    
+    while (isLower)
+    {
+        c +=  c < 'a' ? 26 :  c > 'z' ? - 26 : 0;
+        break;
+    }
+    while (!isLower)
+    {
+        c +=  c < 'A' ? 26 :  c > 'Z' ? - 26 : 0;
+        break;
+    }
+    
     return c;
 }
 
@@ -63,8 +67,14 @@ string & rotate(string &s, int n)
 
 string & caeser(string &s, int n) 
 {
- 
+    string new_s;
     
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (isalpha(s[i])) new_s.push_back(shift_char(s[i],n));
+        else new_s.push_back(s[i]);
+    }
+    return s = new_s;
 }
 
 
@@ -73,13 +83,43 @@ string & caeser(string &s, int n)
 // TASK 3 //
 //--------//
 
-string & substitute(string &s, const string key) {
-
+string & substitute(string &s, const string key) 
+{
+    string new_s;
+    string letters = "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (isalpha(s[i]))
+        {
+            for (int j = 0; j < letters.size(); j++)
+            {
+                if (s[i] == letters[j]) new_s.push_back(key[j]);
+                else if (s[i] == toupper(letters[j])) new_s.push_back(toupper(key[j]));
+            }
+        }
+        else new_s.push_back(s[i]);
+    }
+    return s = new_s;
 }
 
 
-string & unsubstitute(string &s, const string key) {
-
+string & unsubstitute(string &s, const string key) 
+{
+    string new_s;
+    string letters = "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (isalpha(s[i]))
+        {
+            for (int j = 0; j < letters.size(); j++)
+            {
+                if (s[i] == key[j]) new_s.push_back(letters[j]);
+                else if (s[i] == toupper(key[j])) new_s.push_back(toupper(letters[j]));
+            }
+        }
+        else new_s.push_back(s[i]);
+    }
+    return s = new_s;
 }
 
 
@@ -117,9 +157,9 @@ int main(void) {
     vector<int> vkey = {7, 5, -6, 22, -13, 6};
 
     //Uncomment to test the different functions
-    cout << shift_char('c', 2) << endl;
-    cout << shift_char('c', -2) << endl;
-    cout << shift_char('g', 20) << endl;
+    // cout << shift_char('c', 2) << endl;
+    // cout << shift_char('c', -2) << endl;
+    // cout << shift_char('g', 20) << endl;
 
     // cout << rotate(text, 3) << endl;
     // cout << rotate(text, -3) << endl;
@@ -127,8 +167,8 @@ int main(void) {
     // cout << caeser(text, 3) << endl;
     // cout << caeser(text, -3) << endl;
 
-    // cout << substitute(text, key) << endl;
-    // cout << unsubstitute(text, key) << endl;
+    cout << substitute(text, key) << endl;
+    cout << unsubstitute(text, key) << endl;
 
     // cout << vigenere(text, vkey) << endl;
     // cout << unvigenere(text, vkey) << endl;
